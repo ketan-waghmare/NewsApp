8308577f2c6408ba2f12d5093f0e8653d9364b4b
ketan-waghmare <ketan1waghmare@gmail.com>
2021-04-24 22:06:59 +0530

    retrofit sucessfully setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+       const val API_KEY = "3f6031b0bd0f45e6bb3c3d83f7e6b349"
+       const val BASE_URL = "https://newsapi.org"
+    }
+}
