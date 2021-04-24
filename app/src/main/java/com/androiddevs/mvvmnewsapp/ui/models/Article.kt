50fa803c40783544bdd5031ad7d4b0f65fc3ada8
ketan-waghmare <ketan1waghmare@gmail.com>
2021-04-24 22:50:54 +0530

    room database setup sucessfully

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/models/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/models/Article.kt
@@ -1,6 +1,14 @@
-package com.androiddevs.mvvmnewsapp.ui
+package com.androiddevs.mvvmnewsapp.ui.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int? = null,
     val author: String,
     val content: String,
     val description: String,
