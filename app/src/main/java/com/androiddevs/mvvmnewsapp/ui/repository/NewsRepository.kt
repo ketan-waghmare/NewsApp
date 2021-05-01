6ee40208e0ed7eca8561294f213b015baebbfc79
ketan-waghmare <ketan1waghmare@gmail.com>
2021-05-01 13:57:29 +0530

    architechtural skeleton added

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui.repository
+
+import com.androiddevs.mvvmnewsapp.ui.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.ui.models.Article
+
+class NewsRepository(
+    val db : ArticleDatabase
+) {
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ui/NewsViewModel.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui.ui
+
+import androidx.lifecycle.ViewModel
+import com.androiddevs.mvvmnewsapp.ui.repository.NewsRepository
+
+class NewsViewModel(
+    val newsRepository : NewsRepository
+) : ViewModel() {
+}
